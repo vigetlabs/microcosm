fda115662a070b0496c979af626db27569a028f2
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-03-14 11:11:49 -0600

    [revert] Microscope

--- src/Microscope.jsx
+++ src/Microscope.jsx
@@ -1,36 +1,47 @@
-import React from 'react'
-import query from 'query'
+import Pure      from 'react-immutable-render-mixin'
+import React     from 'react/addons'
+import transpose from './transpose'
 
 let Microscope = React.createClass({
-
   propTypes: {
-    flux : React.PropTypes.object.isRequired
+    flux  : React.PropTypes.object.isRequired,
+    watch : React.PropTypes.array.isRequired
   },
 
-  getDefaultProps() {
-    return {
-      element: 'div'
-    }
+  getInitialState() {
+    return this.getState()
   },
 
-  tick() {
-    this.forceUpdate()
+  getState() {
+    let { flux, watch } = this.props
+
+    return watch.reduce(function(memo, key) {
+      memo[key] = flux.stores[key].state
+      return memo
+    }, {})
+  },
+
+  updateState() {
+    this.setState(this.getState())
   },
 
   componentDidMount() {
-    this.props.flux.listen(this.tick)
+    this.props.flux.listen(this.updateState)
   },
 
   componentWillUnmount() {
-    this.props.flux.ignore(this.tick)
+    this.props.flux.ignore(this.updateState)
   },
 
   render() {
-    let { children, element, flux, ...other } = this.props
+    let { children, flux, ...other } = this.props
 
-    return React.createElement(element, other, query(children, flux.stores))
+    let members = React.Children.map(children, Component => {
+      return React.addons.cloneWithProps(Component, { flux, ...this.state })
+    })
+
+    return <div { ...other }>{ members }</div>
   }
-
 })
 
 export default Microscope
