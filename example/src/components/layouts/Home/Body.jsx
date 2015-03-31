d8c5bbfe1076a039527c74f0ee6806d082b03f86
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-03-31 16:33:30 -0400

    [add] A proptype to body.jsx

--- example/src/components/layouts/Home/Body.jsx
+++ example/src/components/layouts/Home/Body.jsx
@@ -4,7 +4,8 @@ import React    from 'react'
 let Body = React.createClass({
 
   propTypes: {
-    lists: React.PropTypes.array.isRequired
+    lists : React.PropTypes.array.isRequired,
+    items : React.PropTypes.array.isRequired
   },
 
   getList(list) {
