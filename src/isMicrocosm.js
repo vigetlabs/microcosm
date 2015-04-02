a4a28a685295302da3cc589dfdfc269ddbad939b
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-04-02 09:47:32 -0400

    [remove] Unused isMicrocosm

--- src/isMicrocosm.js
+++
@@ -1,7 +0,0 @@
-import Microcosm from 'Microcosm'
-
-export default function(props, propName, componentName) {
-  if (props[propName] instanceof Microcosm === false) {
-    throw new Error(`Context type ${ propName } of <${componentName}> should be an instance of Microcosm`)
-  }
-}
