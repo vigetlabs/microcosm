f8199c1397b7f5401b857c9445c6708e3e909a10
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-05-20 07:54:03 -0400

    [update] Tweak bindings for transpilation

--- src/tag.js
+++ src/tag.js
@@ -3,11 +3,11 @@
  * Uniquely tag a function. This is used to identify actions
  */
 
-let uid = 0
+var uid = 0
 
 module.exports = function(fn) {
   if (!fn.hasOwnProperty('toString')) {
-    let mark = uid += 1
+    var mark = uid += 1
     fn.toString = () => 'microcosm_tagged_' + mark
   }
 }
