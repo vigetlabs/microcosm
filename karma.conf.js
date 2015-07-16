2baf7de2d51053e3cb28d637acf83e41473b714f
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-07-16 08:36:06 -0400

    Format karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,6 @@
-var Webpack        = require('webpack')
 var webpack_config = require('./webpack.config')
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     browsers: [ 'Chrome' ],
 
@@ -14,9 +13,9 @@ module.exports = function(config) {
     ],
 
     preprocessors: {
-      'src/**/__tests__/*.js*' : [ 'webpack', 'sourcemap' ],
-      'examples/**/__tests__/*.js*' : [ 'webpack', 'sourcemap' ],
-      'addons/**/__tests__/*.js*' : [ 'webpack', 'sourcemap' ]
+      'src/**/__tests__/*.js*': [ 'webpack', 'sourcemap' ],
+      'examples/**/__tests__/*.js*': [ 'webpack', 'sourcemap' ],
+      'addons/**/__tests__/*.js*': [ 'webpack', 'sourcemap' ]
     },
 
     reporters: [ 'mocha', 'coverage' ],
@@ -26,24 +25,24 @@ module.exports = function(config) {
     },
 
     coverageReporter: {
-      dir    : process.env.CIRCLE_ARTIFACTS || 'coverage',
-      type   : 'html',
-      subdir : '.'
+      dir: process.env.CIRCLE_ARTIFACTS || 'coverage',
+      type: 'html',
+      subdir: '.'
     },
 
     webpack: {
-      devtool : 'inline-source-map',
-      resolve : webpack_config.resolve,
+      devtool: 'inline-source-map',
+      resolve: webpack_config.resolve,
 
       module: {
         noParse: webpack_config.module.noParse,
         loaders: [{
-          test    : /\.jsx*$/,
-          exclude : /node_modules/,
-          loader  : 'babel',
-          query   : {
-            auxiliaryCommentBefore: "istanbul ignore next",
-            optional: ["runtime"]
+          test: /\.jsx*$/,
+          exclude: /node_modules/,
+          loader: 'babel',
+          query: {
+            auxiliaryCommentBefore: 'istanbul ignore next',
+            optional: ['runtime']
           }
         }],
         postLoaders: [
@@ -59,5 +58,5 @@ module.exports = function(config) {
     webpackServer: {
       noInfo: true
     }
-  });
-};
+  })
+}
