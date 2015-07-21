8a753731cdd97f0cba7a2e6db15e8263538012e6
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-07-21 08:54:36 -0400

    Clean up and document async.js

--- src/async.js
+++ src/async.js
@@ -1,5 +1,6 @@
 /**
- * Chain generator calls using nodeify
+ * Take a value, be it primitive, a promise, or a generator, and reduce it down
+ * into chunks processable by transactions.
  */
 
 let isGenerator = require('is-generator')
@@ -7,26 +8,30 @@ let isPromise = require('is-promise')
 
 const DEFAULT_ERROR = new Error('Rejected Promise')
 
-function onSuccess(callback, body) {
-  return callback(null, body, true)
-}
-
-function onFailure(callback, error) {
-  callback(error || DEFAULT_ERROR, null, true)
-
-  // Throw so future error handling can occur
-  throw error
-}
-
+/**
+ * If a given value is a promise, wait for it and
+ * execute a callback using the error-first convention.
+ *
+ * @param {any} promise - The target value
+ * @param {Function} callback - An error-first callback
+ */
 function nodeify (promise, callback) {
   if (!isPromise(promise)) {
     return callback(null, promise, true)
   }
 
-  return promise.then(onSuccess.bind(null, callback),
-                      onFailure.bind(null, callback))
+  return promise.then(body => callback(null, body, true), function(error) {
+    callback(error || DEFAULT_ERROR, null, true)
+    throw error
+  })
 }
 
+/**
+ * Given an iterator, call nodeify sequentially on each iteration
+ *
+ * @param {Iterator} iterator - An iterable collection
+ * @param {Function} callback - An error-first callback
+ */
 function waterfall (iterator, callback) {
   return nodeify(iterator.next().value, function step (error, body) {
     let { done, value } = iterator.next()
@@ -37,6 +42,13 @@ function waterfall (iterator, callback) {
   })
 }
 
+/**
+ * Checks if a given value is a generator. If so, process it sequentially.
+ * Otherwise resolve the specific value using `nodeify`.
+ *
+ * @param {any} value - The compared value
+ * @param {Function} callback - An error-first callback
+ */
 function chain (value, callback) {
   return (isGenerator(value) ? waterfall : nodeify)(value, callback)
 }
