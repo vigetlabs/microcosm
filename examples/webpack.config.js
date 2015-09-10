3a233e8536c708af503ad7a949a75d5c4588197b
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-09-10 08:21:59 -0400

    Update examples, dev dependencies, and webpack config

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,41 +1,46 @@
 var webpack = require('webpack')
-var config  = Object.create(require('../webpack.config'))
 var path    = require('path')
 
-config.context   = __dirname
-config.devtool   = 'inline-source-map'
-config.externals = {}
-
-config.entry = {
-  'advanced' : './advanced/src/index',
-  'simple-svg' : './simple-svg/index',
-  'chatbot' : './chatbot/index'
-}
-
-config.output = {
-  filename: '[name].js',
-  path: path.join(__dirname, 'assets', 'js')
-}
-
-config.postcss = [ require('autoprefixer-core') ]
-
-config.module.loaders = [
-  {
-    test     : /\.jsx*$/,
-    exclude  : /node_modules/,
-    loader   : 'babel',
-    query    : {
-      optional : ['runtime']
-    }
+module.exports = {
+  context: __dirname,
+  devtool: 'inline-source-map',
+  entry: {
+    'advanced' : './advanced/src/index',
+    'simple-svg' : './simple-svg/index',
+    'chatbot' : './chatbot/index'
   },
-  {
-    test    : /\.s*(c|a)ss$/,
-    loader  : 'style!css!postcss!sass'
+
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'assets', 'js')
   },
-  {
-    test    : /\.(svg)$/,
-    loader  : 'raw'
+
+  resolve: {
+    extensions: [ '', '.js', '.jsx', '.json', '.scss', '.svg' ],
+    modulesDirectories: [ 'web_modules', 'node_modules', 'src', 'examples/advanced', '..' ]
+
+  },
+
+  postcss: [ require('autoprefixer') ],
+
+  module: {
+    loaders: [
+      {
+        test     : /\.jsx*$/,
+        exclude  : /node_modules/,
+        loader   : 'babel',
+        query    : {
+          optional : ['runtime']
+        }
+      },
+      {
+        test    : /\.s*(c|a)ss$/,
+        loader  : 'style!css!postcss!sass'
+      },
+      {
+        test    : /\.(svg)$/,
+        loader  : 'raw'
+      }
+    ]
   }
-]
-
-module.exports = config
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,11 @@
 var Webpack = require('webpack')
 
 module.exports = {
-  resolve: {
-    extensions: [ '', '.js', '.jsx', '.json', '.scss', '.svg' ],
-    modulesDirectories: [ 'web_modules', 'node_modules', 'src', 'examples/advanced', __dirname ]
-  },
-
-  externals: {
-    react: 'React'
-  },
 
   plugins: [
     new Webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
-  ],
+  ]
 
-  module: {
-    noParse: [ 'react' ],
-    loaders: [{
-      test     : /\.jsx*$/,
-      exclude  : /node_modules/,
-      loader   : 'babel'
-    }]
-  }
 }
