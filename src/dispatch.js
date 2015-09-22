a04c54f96eebbc7ae12cf2fe15ccb969ffc98368
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-09-22 08:58:12 -0400

    Use let instead of var

--- src/dispatch.js
+++ src/dispatch.js
@@ -13,9 +13,9 @@ module.exports = function dispatch(stores, state, transaction) {
   let next = Object.assign({}, state)
 
   for (var i = 0; i < stores.length; i++) {
-    var key   = stores[i][0]
-    var store = stores[i][1]
-    var answer = send(store, key, next, transaction)
+    let key   = stores[i][0]
+    let store = stores[i][1]
+    let answer = send(store, key, next, transaction)
 
     if (answer !== void 0) {
       next[key] = answer
