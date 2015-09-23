acb58b0a298c361b275504dd9921b0242cbc6e03
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-09-23 08:02:34 -0400

    Add developer warning in tag when not a function

--- src/tag.js
+++ src/tag.js
@@ -6,6 +6,10 @@
 let uid = 0
 
 module.exports = function tag (fn) {
+  if (process.env.NODE_ENV !== 'production' && (typeof fn !== 'function')) {
+    throw TypeError('Attempted to tag an action, but the provided value is not a function. Instead got ' + typeof fn)
+  }
+
   let name = fn.name || 'microcosm_action'
   let mark = uid++
 
