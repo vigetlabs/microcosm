554444ebff45c83afb54dc03d6917fad6079ad9d
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-10-06 08:32:48 -0400

    Add more documentation to tree-performance

--- benchmarks/tree-performance.js
+++ benchmarks/tree-performance.js
@@ -1,48 +1,86 @@
-require('console.table')
+/**
+ * Tree Performance Benchmark
+ */
 
 var Tree  = require(__dirname + '/../dist/src/Tree')
+var merge = require(__dirname + '../dist/src/merge')
 var time  = require('microtime')
 var SIZE  = 10000
 var stats = { build: 0, root: 0, merge: 0, size: 0, prune: 0, memory: 0 }
+var tree  = new Tree()
 
-var tree = new Tree()
-
-// Do not allow GC to get in the way of tests
+/**
+ * Force garbage collection. This is exposed by invoking
+ * node with --expose-gc. This allows us to record heap usage
+ * before and after the test to check for memory leakage
+ */
 global.gc()
+
 var memoryBefore = process.memoryUsage().heapUsed
 
+
+/**
+ * Measure the average append time.
+ */
 var now = time.now()
 for (var k = SIZE; k >= 0; k--) {
   tree.append({ one: 'one', two: 'two', three: 'three' })
 }
-stats.build = (time.now() - now) / 1000
+stats.build = ((time.now() - now) / 1000) / SIZE
 
+/**
+ * Measure the time it takes to bubble up to the root
+ * of the tree from the current focal point.
+ */
 now = time.now()
 tree.root()
 stats.root = (time.now() - now) / 1000
 
+
+/**
+ * Measure the time it takes to determine the size of
+ * the current branch. This should loosely be in line with
+ * ::root()
+ */
 now = time.now()
 tree.size()
 stats.size = (time.now() - now) / 1000
 
+
+/**
+ * Measure time to merge together all nodes. This is a useful
+ * gut check of the potential fastest possible merger of objects
+ * within Microcosm's dispatch process.
+ */
 now = time.now()
-tree.reduce(function(a, b) {
-  for (var key in b) {
-    a[key] = b[key]
-  }
-  return a
-}, {})
+tree.reduce(merge, {})
 stats.merge = (time.now() - now) / 1000
 
+
+/**
+ * Measure time to dispose all nodes in the tree. This also has
+ * the side effect of helping to test memory leakage later.
+ */
 now = time.now()
 tree.prune(function() {
   return true
 })
 stats.prune = (time.now() - now) / 1000
 
+
+/**
+ * Now that the tree has been pruned, force garbage collection
+ * and record the increase in heap usage.
+ */
 global.gc()
 var memoryAfter = process.memoryUsage().heapUsed
 stats.memory = ((memoryAfter - memoryBefore) / memoryBefore) * 100
+
+
+/**
+ * Finally, report our findings.
+ */
+require('console.table')
 
 console.table([{
   'Nodes': SIZE,
@@ -56,9 +94,10 @@ console.table([{
 
 
 /**
- * Test a few things
+ * As a final check, assert that memory usage hasn't grown significantly.
+ * Some growth is to be expected, however anything above 2% is a clear failure
+ * of `prune()` to dispose of nodes.
  */
-
 var assert = require('assert')
 
 assert(stats.memory < 2, 'Memory should not exceed 2% increase')
