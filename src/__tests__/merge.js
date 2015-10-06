719085d8a896bbcb39c775d5ee96388952a174c2
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-10-06 07:28:19 -0400

    Add test coverage for merge

---
+++ src/__tests__/merge.js
@@ -0,0 +1,25 @@
+import assert from 'assert'
+import merge  from '../merge'
+
+describe('merge', function() {
+
+  it ('will only merge into a simple object', function() {
+    assert.throws(function() {
+      merge([], {})
+    })
+  })
+
+  it ('will only merge from simple object', function() {
+    assert.throws(function() {
+      merge({}, [])
+    })
+  })
+
+  it ('merges keys into a target', function() {
+    let sample = merge({ one: 'one' }, { two: 'two' })
+
+    assert.equal(sample.one, 'one')
+    assert.equal(sample.two, 'two')
+  })
+
+})
