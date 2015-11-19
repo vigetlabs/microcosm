25912a05bfb85d513072fe515599a0e9e5dc65fe
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-11-19 14:45:50 -0500

    Use assert.ifError instead of conditional in server index

--- examples/server/index.js
+++ examples/server/index.js
@@ -6,11 +6,10 @@
 require('babel/register')
 
 var Server = require('./lib/server')
+var assert = require('assert')
 
 Server.start(process.env.PORT || 4000, function (error, server) {
-  if (error) {
-    throw Error(error)
-  }
+  assert.ifError(error)
 
   console.log('[info] Examples listening on %s', server.info.uri)
 })
