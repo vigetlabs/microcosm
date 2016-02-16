86261f08040bc2ad9d6e68b3c8a31602bed7eb3b
Nate Hunzaker <nate.hunzaker@gmail.com>
2016-02-16 08:10:55 -0500

    Add mutation test to cover in-place updates

---
+++ test/mutation-test.js
@@ -0,0 +1,36 @@
+let Microcosm = require('../src/Microcosm')
+let assert    = require('assert')
+
+describe('Mutation', function() {
+
+  context('when a store writes mutatively', function() {
+    let action = function() {}
+
+    beforeEach(function(done) {
+      this.app = new Microcosm()
+
+      this.app.addStore(function() {
+        return {
+          getInitialState() {
+            return { test: false }
+          },
+          [action](state) {
+            state.test = true
+            return state
+          }
+        }
+      })
+
+      this.app.start(done)
+    })
+
+
+    it ('it writes to application state', function (done) {
+      this.app.push(action, true, error => {
+        assert.equal(this.app.state.test, true)
+        done(error)
+      })
+    })
+  })
+
+})
