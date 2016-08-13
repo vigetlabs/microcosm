b0306995d2cc1d094d62156795202babd3a776e9
Nate Hunzaker <nate.hunzaker@gmail.com>
2016-08-13 13:23:23 -0400

    Fix some old names and options in painter example

--- examples/painter/app/painter.js
+++ examples/painter/app/painter.js
@@ -2,10 +2,9 @@ import Microcosm from '../../../src/microcosm'
 import Pixels    from './stores/pixels'
 
 export default class Painter extends Microcosm {
+  constructor(options) {
+    super(options)
 
-  constructor() {
-    super({ maxHistory: Infinity })
     this.addStore('pixels', Pixels)
   }
-
 }
