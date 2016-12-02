956f127e1a2128af7091ddad5d67c8b8587c563d
Nathan Hunzaker <nate.hunzaker@gmail.com>
2016-12-02 14:41:53 -0500

    Use inline prop form for view in chat bot

--- examples/chatbot/app/presenters/chat.jsx
+++ examples/chatbot/app/presenters/chat.jsx
@@ -1,17 +1,12 @@
-import React     from 'react'
 import Presenter from '../../../../src/addons/presenter'
 import Messenger from '../views/messenger'
 
 export default class ChatPresenter extends Presenter {
+  view = Messenger
 
   model () {
     return {
       messages : state => state.messages
     }
   }
-
-  view ({ messages }) {
-    return <Messenger messages={ messages } />
-  }
-
 }
