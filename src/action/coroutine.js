20311cc551b102e014a5021aad16eb0fe830457f
Nathan Hunzaker <nate.hunzaker@gmail.com>
2016-12-02 16:50:34 -0500

    Collapse coroutine

--- src/action/coroutine.js
+++ src/action/coroutine.js
@@ -1,23 +1,47 @@
-import promise from './middleware/promise'
-import thunk   from './middleware/thunk'
+function isPromise(obj, type) {
+  return !!obj && (type === 'object' || type === 'function') && typeof obj.then === 'function'
+}
 
 /**
  * Coroutine is used by an action to determine how it should resolve the
  * body of their associated behavior.
- *
- * @param {Action} action - The target action
- * @param {Any} body - The resulting value of the action's behavior
- * @return {Action} the provided action
  * @private
  */
 export default function coroutine (action, body) {
-  if (promise.condition(body)) {
-    return promise.call(action, body)
+  let type = typeof body
+
+  /**
+   * Provide support for Promises:
+   *
+   * 1. Open the action
+   * 2. Unwrap the promise using `setTimeout`, which prevents errors
+   *    elsewhere in the dispatch execution process from being trapped.
+   * 3. If the promise is rejected, reject the action
+   * 4. Otherwise resolve the action with the returned body
+   */
+  if (isPromise(body, type)) {
+    action.open()
+
+    body.then(
+      result => global.setTimeout(() => action.resolve(result), 0),
+      error  => global.setTimeout(() => action.reject(error), 0)
+    )
+
+    return action
   }
 
-  if (thunk.condition(body)) {
-    return thunk.call(action, body)
+  /**
+   * Check for thunks. An escape hatch to direction work with an
+   * action. It is triggered by returning a function from a
+   * behavior. This middleware will execute that function with the
+   * action as the first argument.
+   */
+  if (type === 'function') {
+    body(action)
+
+    return action
   }
 
+  // Otherwise just return a resolved action
   return action.resolve(body)
 }
