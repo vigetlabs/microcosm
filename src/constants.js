d80cb578e45e8a140643f365c910e0402a73cd23
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-02-22 08:21:22 -0500

    Support action type aliases in nested registers
    Also increase test coverage for registration

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,7 @@
  * Actions move through a specific set of states. This manifest
  * controls how they should behave.
  */
+
 export const ACTION_STATES = [
   { key: 'open', disposable: false, once: true, listener: 'onOpen' },
   { key: 'update', disposable: false, once: false, listener: 'onUpdate' },
@@ -9,3 +10,17 @@ export const ACTION_STATES = [
   { key: 'reject', disposable: true, once: true, listener: 'onError' },
   { key: 'cancel',  disposable: true, once: true, listener: 'onCancel' }
 ]
+
+// For nested registrations, track our aliases
+export const ACTION_ALIASES = {
+  inactive  : 'inactive',
+  open      : 'open',
+  update    : 'loading',
+  loading   : 'update',
+  done      : 'resolve',
+  resolve   : 'done',
+  reject    : 'error',
+  error     : 'reject',
+  cancel    : 'cancelled',
+  cancelled : 'cancel'
+}
--- src/get-registration.js
+++ src/get-registration.js
@@ -1,14 +1,25 @@
+import { ACTION_ALIASES } from './constants'
+
+const DONE = 'done'
+
+function isObject (target) {
+  return target && typeof target === 'object'
+}
+
 export default function getRegistration (entity, behavior, status) {
-  let type = behavior[status]
+  var alias = status ? ACTION_ALIASES[status] : DONE
+
+  if (alias == null) {
+    throw new ReferenceError('Invalid action status ' + status)
+  }
 
   if (entity.register) {
     var registry = entity.register()
 
-    if (typeof registry[behavior] === 'object') {
-
-      return registry[behavior][status]
+    if (isObject(registry[behavior])) {
+      return registry[behavior][alias] || registry[behavior][status]
     } else {
-      return registry[type]
+      return registry[behavior[alias]]
     }
   }
 
