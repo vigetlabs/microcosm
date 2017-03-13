e47b672da997f24f1ecd117d93f736e40dd4cdbd
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-03-13 14:37:25 -0500

    Add test coverage for lifecycle methods

---
+++ test/unit/microcosm/lifecycle.test.js
@@ -0,0 +1,24 @@
+import Microcosm from '../../../src/microcosm'
+
+import {
+  BIRTH,
+  START
+} from '../../../src/lifecycle'
+
+describe('Lifecycle', function () {
+
+  it('$birth should never be invoked directly', function () {
+    let repo = new Microcosm()
+    let test = repo.prepare(BIRTH)
+
+    expect(test).toThrow()
+  })
+
+  it('$start should never be invoked directly', function () {
+    let repo = new Microcosm()
+    let test = repo.prepare(START)
+
+    expect(test).toThrow()
+  })
+
+})
