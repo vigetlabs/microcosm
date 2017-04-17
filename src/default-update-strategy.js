69435f8a30f8b2649a8689897472b59dfd4f15c3
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-04-17 07:28:19 -0400

    Replace ric with timeout fallback

--- src/default-update-strategy.js
+++ src/default-update-strategy.js
@@ -4,16 +4,28 @@
  * Microcosm.
  */
 
-// requestIdleCallback isn't supported everywhere
-import 'ric'
+/**
+ * requestIdleCallback isn't supported everywhere and is hard to
+ * polyfill. For environments that do not support it, just use a
+ * setTimeout.
+ *
+ * Note: To be fully compliant, we would invoke the callback with the
+ * time remaining. Given our usage, we don't need to do that.
+ */
+const scheduler = global.requestIdleCallback || (update => setTimeout(update, 4))
 
-let batchOptions = { timeout: 36 }
+/**
+ * When using requestIdleCallback, batch together updates until the
+ * browser is ready for them, but never make the user wait longer than
+ * 36 milliseconds.
+ */
+const BATCH_OPTIONS = { timeout: 36 }
 
 export default function defaultUpdateStrategy (options) {
 
   return update => {
     if (options.batch === true) {
-      global.requestIdleCallback(update, batchOptions)
+      scheduler(update, BATCH_OPTIONS)
     } else {
       update()
     }
--- test/helpers/mock-send.js
+++ test/helpers/mock-send.js
@@ -2,10 +2,7 @@ export default function mockPresenter (send = jest.fn()) {
   send.context = { send }
 
   send.childContextTypes = {
-    send: (props, propName) => {
-      console.assert(typeof props[propName] === 'function')
-      return null
-    }
+    send: () => null
   }
 
   return send
