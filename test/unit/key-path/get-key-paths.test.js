1e546aff5d643e21fe1d0d72a1346dc43e6de65e
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-04-21 07:37:11 -0400

    Update formatting of key path test

--- test/unit/key-path/get-key-paths.test.js
+++ test/unit/key-path/get-key-paths.test.js
@@ -1,10 +1,7 @@
-import {
-  getKeyPaths
-} from '../../../src/key-path'
+import { getKeyPaths } from '../../../src/key-path'
 
-describe('getKeyPaths', function () {
-
-  it('removes leading spaces between paths', function () {
+describe('getKeyPaths', function() {
+  it('removes leading spaces between paths', function() {
     let path = getKeyPaths('foo, bar')
 
     expect(path.length).toBe(2)
@@ -13,7 +10,7 @@ describe('getKeyPaths', function () {
     expect(path[1]).toEqual(['bar'])
   })
 
-  it('removes trailing spaces between paths', function () {
+  it('removes trailing spaces between paths', function() {
     let path = getKeyPaths('foo ,bar')
 
     expect(path.length).toBe(2)
@@ -22,7 +19,7 @@ describe('getKeyPaths', function () {
     expect(path[1]).toEqual(['bar'])
   })
 
-  it('removes spaces at the beginning of a path', function () {
+  it('removes spaces at the beginning of a path', function() {
     let path = getKeyPaths(' foo,bar')
 
     expect(path.length).toBe(2)
@@ -31,7 +28,7 @@ describe('getKeyPaths', function () {
     expect(path[1]).toEqual(['bar'])
   })
 
-  it('removes spaces at the end of a path', function () {
+  it('removes spaces at the end of a path', function() {
     let path = getKeyPaths('foo,bar ')
 
     expect(path.length).toBe(2)
@@ -40,7 +37,7 @@ describe('getKeyPaths', function () {
     expect(path[1]).toEqual(['bar'])
   })
 
-  it('does not remove space between words', function () {
+  it('does not remove space between words', function() {
     let path = getKeyPaths('foo, b ar')
 
     expect(path.length).toBe(2)
@@ -48,5 +45,4 @@ describe('getKeyPaths', function () {
     expect(path[0]).toEqual(['foo'])
     expect(path[1]).toEqual(['b ar'])
   })
-
 })
