3f055318d2f363241bad6e4e705efdcf7b457795
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-06-02 08:18:35 -0400

    Ensure test coverage of infinite loop case with repo.register

--- test/unit/microcosm/register.test.js
+++ test/unit/microcosm/register.test.js
@@ -1,7 +1,7 @@
 import Microcosm, { update } from '../../../src/microcosm'
 
 describe('Microcosm::register', function() {
-  it('a Microcosm can register to actiosn', function() {
+  it('a Microcosm can register to actions', function() {
     const ADD = 'ADD'
 
     class Repo extends Microcosm {
@@ -26,5 +26,52 @@ describe('Microcosm::register', function() {
     repo.push(ADD, 2)
 
     expect(repo).toHaveState('count', 4)
+  })
+
+  describe('lifecycle hooks', function() {
+    it('does not call deserialize infinitely', function() {
+      class Repo extends Microcosm {
+        register() {
+          return {}
+        }
+      }
+
+      let repo = new Repo()
+      let spy = jest.spyOn(repo, 'deserialize')
+
+      repo.deserialize()
+
+      expect(spy).toHaveBeenCalledTimes(1)
+    })
+
+    it('does not call serialize infinitely', function() {
+      class Repo extends Microcosm {
+        register() {
+          return {}
+        }
+      }
+
+      let repo = new Repo()
+      let spy = jest.spyOn(repo, 'serialize')
+
+      repo.serialize()
+
+      expect(spy).toHaveBeenCalledTimes(1)
+    })
+
+    it('does not call getInitialState infinitely', function() {
+      class Repo extends Microcosm {
+        register() {
+          return {}
+        }
+      }
+
+      let repo = new Repo()
+      let spy = jest.spyOn(repo, 'getInitialState')
+
+      repo.getInitialState()
+
+      expect(spy).toHaveBeenCalledTimes(1)
+    })
   })
 })
