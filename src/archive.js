03101e787d6db6b1aa8898a5a7912298f88e89ee
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-06-15 07:51:30 -0400

    Add Flow
    This commit adds FlowType to Microcosm. Additionally, it implements
    types for the internal archive package.

--- src/archive.js
+++ src/archive.js
@@ -1,9 +1,16 @@
 /**
  * @fileoverview Keeps track of prior action states according to an
  * action's id
+ * @flow
  */
 
+import type Action from './action'
+
+type Payload = { [string]: * }
+
 class Archive {
+  pool: { [string]: Payload }
+
   /**
    * @property {Object} pool An object mapping of action ids to snapshots
    */
@@ -16,7 +23,7 @@ class Archive {
    * parent.
    * @param {Action} action Action to create an initial snapshot for
    */
-  create(action) {
+  create(action: Action): void {
     this.set(action, this.get(action.parent))
   }
 
@@ -24,12 +31,21 @@ class Archive {
    * Access a prior snapshot for a given action
    * @param {Action} action Action for requested snapshot
    */
-  get(action, fallback) {
+  get(action: Action, fallback: ?Payload): Payload {
     console.assert(action, 'Unable to get ' + action + ' action')
 
-    let value = this.pool[action.id]
+    if (this.has(action)) {
+      return this.pool[action.id]
+    }
 
-    return value === undefined ? fallback : value
+    return fallback == null ? {} : fallback
+  }
+
+  /**
+   * Does a snapshot exist for an action?
+   */
+  has(action:Action):boolean {
+    return typeof this.pool[action.id] !== 'undefined'
   }
 
   /**
@@ -37,7 +53,7 @@ class Archive {
    * @param {Action} action Action for requested snapshot
    * @param {Object} snapshot
    */
-  set(action, snapshot) {
+  set(action: Action, snapshot: Payload) {
     this.pool[action.id] = snapshot
   }
 
@@ -45,7 +61,7 @@ class Archive {
    * Remove a snapshot for an action.
    * @param {Action} action Action to eliminate snapshot for
    */
-  remove(action) {
+  remove(action: Action): void {
     console.assert(action, 'Unable to remove ' + action + ' action.')
 
     delete this.pool[action.id]
--- test/integration/reconciliation.test.js
+++ test/integration/reconciliation.test.js
@@ -78,7 +78,7 @@ describe('Reconciliation', function() {
     one.resolve()
 
     // Should be gone
-    expect(repo.recall(one)).toBeUndefined()
+    expect(repo.recall(one, false)).toBe(false)
     // We need two for rolling forward changes from three
     expect(repo.recall(two)).toBeDefined()
     // We need three because it is the root
