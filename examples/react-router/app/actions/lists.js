6c80b3c79a8dec001a8e884e672ac405dfa61eae
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-06-28 16:46:26 -0400

    Drop param

--- examples/react-router/app/actions/lists.js
+++ examples/react-router/app/actions/lists.js
@@ -2,7 +2,7 @@
 import uid from 'uid'
 
 export function addList(params: Object) {
-  return { id: uid(), ...params, foo: 'bar' }
+  return { id: uid(), ...params }
 }
 
 export function removeList(id: number) {
