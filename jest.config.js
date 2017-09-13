7e47b030432994ca0743db34859118356d344e30
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-09-13 15:35:38 -0400

    Replace nvm with engines

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  verbose: true,
-  projects: [
-    '<rootDir>/packages/microcosm',
-    '<rootDir>/packages/microcosm-preact'
-  ],
-  testPathIgnorePatterns: ['build', 'examples', 'example', 'node_modules'],
-  roots: ['<rootDir>/packages/microcosm', '<rootDir>/packages/microcosm-preact']
-}
