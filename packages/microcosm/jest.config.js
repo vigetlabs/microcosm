f908d4883f61626286d14ebe9f1f7add6ba076a3
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-09-26 14:25:33 -0400

    Upgrade to React 16

--- packages/microcosm/jest.config.js
+++ packages/microcosm/jest.config.js
@@ -2,6 +2,7 @@ const { moduleNameMapper } = require('../../jest.config')
 
 module.exports = {
   setupTestFrameworkScriptFile: './test/helpers/setup.js',
+  setupFiles: ['raf/polyfill'],
   modulePathIgnorePatterns: ['<rootDir>/build'],
   moduleNameMapper: moduleNameMapper
 }
