08c2a8626f09768df8ab233378e9481e8e1e607c
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-09-28 14:50:04 -0400

    Ensure action.link executes in correct order

--- test/unit/action/link.test.js
+++ test/unit/action/link.test.js
@@ -1,15 +1,21 @@
 import Microcosm from '../../../src/microcosm'
 
+function delay(payload, n) {
+  return new Promise(resolve => setTimeout(() => resolve(payload), n))
+}
+
 describe('Action link', function() {
-  it('remembers and returns the results of each child action', function() {
+  it('remembers and returns the results of each child action', async function() {
     const repo = new Microcosm()
 
     const action = repo.parallel([
-      repo.push(() => 1),
-      repo.push(() => 2),
-      repo.push(() => 3)
+      repo.push(() => delay(1, 20)),
+      repo.push(() => delay(2, 10)),
+      repo.push(() => delay(3, 5))
     ])
 
-    expect(action.payload).toEqual([1, 2, 3])
+    let payload = await action
+
+    expect(payload).toEqual([1, 2, 3])
   })
 })
