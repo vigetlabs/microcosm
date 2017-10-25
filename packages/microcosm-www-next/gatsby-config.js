d5c207865890073826da2c06b1eb3b60c935819e
Chloe Hwang <c.chloeh@gmail.com>
2017-10-25 10:17:56 -0400

    finished setting up sass/gatsby

--- packages/microcosm-www-next/gatsby-config.js
+++ packages/microcosm-www-next/gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`
+  ],
 }
--- packages/microcosm-www-next/src/layouts/index.js
+++ packages/microcosm-www-next/src/layouts/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+import './index.scss'
 
 const Header = () => (
   <div
