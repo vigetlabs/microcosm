01076ec21c0b55b4387acdd13f4b69353bdfa62e
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-11-09 08:47:43 -0500

    Ignore test helpers when collecting code coverage

--- packages/microcosm/jest.config.js
+++ packages/microcosm/jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   setupTestFrameworkScriptFile: './test/helpers/setup.js',
   setupFiles: ['raf/polyfill'],
   modulePathIgnorePatterns: ['<rootDir>/build'],
+  coveragePathIgnorePatterns: ["/node_modules/", "/test/helpers/"],
   moduleNameMapper: moduleNameMapper
 }
