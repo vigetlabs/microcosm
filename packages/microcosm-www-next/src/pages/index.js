1896dfba06b3f16ffe3c36f94a627908fed50829
Chloe Hwang <c.chloeh@gmail.com>
2017-11-29 15:45:25 -0500

    trying out two columns layout

--- packages/microcosm-www-next/src/pages/index.js
+++ packages/microcosm-www-next/src/pages/index.js
@@ -2,6 +2,66 @@ import React from 'react'
 
 const IndexPage = () => (
   <section className="section">
+    <div className="wrapper">
+
+      <div className="col-left">
+        {/* <h2 className="section-title">
+          <span>01.</span>
+          Rendering a View
+        </h2> */}
+        <figure className="figure -left">
+          <img src="" className="figure__graphic" alt="TODO" />
+          <figcaption className="figure__content">
+            <h3 className="figure__content__header">In the browser</h3>
+            <p className="figure__content__text">
+              A user fires up the browser to take a quiz.
+            </p>
+          </figcaption>
+        </figure>
+      </div>
+
+      <div className="col-right">
+        <figure className="figure -right">
+          <img src="" className="figure__graphic" alt="TODO" />
+          <figcaption className="figure__content">
+            <h3 className="figure__content__header">In Microcosm</h3>
+            <p className="figure__content__text">
+              The <a href="TODO">Domains</a> are in charge of keeping state
+              organized, and provide whatever data is necessary to the
+              Presenter. A Presenter at it's core is a React Component, so it
+              uses the data it pulls from the Domains to render the appropriate
+              view.
+            </p>
+          </figcaption>
+        </figure>
+        <figure className="figure -right">
+          <img src="" className="figure__graphic" alt="TODO" />
+          <figcaption className="figure__content">
+            <h3 className="figure__content__header">In Microcosm</h3>
+            <p className="figure__content__text">
+              The <a href="TODO">Domains</a> are in charge of keeping state
+              organized, and provide whatever data is necessary to the
+              Presenter. A Presenter at it's core is a React Component, so it
+              uses the data it pulls from the Domains to render the appropriate
+              view.
+            </p>
+          </figcaption>
+        </figure>
+        <figure className="figure -right">
+          <img src="" className="figure__graphic" alt="TODO" />
+          <figcaption className="figure__content">
+            <h3 className="figure__content__header">In Microcosm</h3>
+            <p className="figure__content__text">
+              The <a href="TODO">Domains</a> are in charge of keeping state
+              organized, and provide whatever data is necessary to the
+              Presenter. A Presenter at it's core is a React Component, so it
+              uses the data it pulls from the Domains to render the appropriate
+              view.
+            </p>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
     {/* <div className="wrapper">
       <h2 className="section-title">
         <span>01.</span>
@@ -35,7 +95,7 @@ const IndexPage = () => (
       </div>
     </div> */}
 
-    <div className="col-left">
+    {/* <div className="col-left">
       <div className="wrapper2">
         <figure className="figure -left">
           <img src="" className="figure__graphic" alt="TODO" />
@@ -65,7 +125,7 @@ const IndexPage = () => (
           </figcaption>
         </figure>
       </div>
-    </div>
+    </div> */}
   </section>
 )
 
