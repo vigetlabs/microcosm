3b3973b98cf8cd6c7d56a3da07a2405773683c9b
Chloe Hwang <c.chloeh@gmail.com>
2017-12-14 16:12:32 -0500

    implementing intersection observer to change text on scroll

--- packages/microcosm-www-next/src/components/index.js
+++ packages/microcosm-www-next/src/components/index.js
@@ -1,4 +1,4 @@
-import Section1 from './section1';
-import Section2 from './section2';
+import Graphic1 from './graphic1';
+import Graphic2 from './graphic2';
 
-export { Section1, Section2 };
+export { Graphic1, Graphic2 };
