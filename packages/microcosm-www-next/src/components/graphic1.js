8d885c146e418bbe2532e9a7c28acabb960542a0
Chloe Hwang <c.chloeh@gmail.com>
2017-12-14 18:15:17 -0500

    added functionality to change view by button and started refactoring

--- packages/microcosm-www-next/src/components/graphic1.js
+++ packages/microcosm-www-next/src/components/graphic1.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const Graphic1 = ({ fill }) => (
-  <figure
-    id="graphic1"
-    className="section__graphic__figure"
-    data-module="ObserveGraphic"
-  >
-    <img src="" alt="TODO - image" />
+  <figure>
+    <img
+      src="http://via.placeholder.com/600x600"
+      alt="TODO"
+      className="section__graphic__figure"
+      data-module="ObserveGraphic"
+      data-section="1"
+    />
   </figure>
 )
 
