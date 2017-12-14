c51c5d6ece9302b8bb17f93bfc00671251943df6
Chloe Hwang <c.chloeh@gmail.com>
2017-12-14 18:15:17 -0500

    added functionality to change view by button and started refactoring

--- packages/microcosm-www-next/src/pages/index.js
+++ packages/microcosm-www-next/src/pages/index.js
@@ -8,33 +8,87 @@ export default class IndexPage extends React.Component {
   }
 
   componentDidMount() {
-    // do accessibility to alert to change!!!!
-    this.minThreshold = 0.6;
-    this.lastIntersectingEntry = null;
-    this.sectionHeading = document.getElementById('section__content__heading');
-    let graphics = document.querySelectorAll('[data-module="ObserveGraphic"]');
-
-    this.beginObserve(graphics);
+    // TODO - accessibility to alert on change
+    this.setVars();
+    this.beginObserve();
   }
 
-  beginObserve(graphics) {
+  setVars() {
+    this.onMicrocosmView = true;
+    this.currentSection = 1;
+    this.minThreshold = 0.6;
+    this.text = document.getElementById('text');
+    this.heading = document.getElementById('heading');
+    this.subheadingTop = document.getElementById('subheading-top');
+    this.subheadingBottom = document.getElementById('subheading-bottom');
+
+    let graphics = document.querySelectorAll('[data-module="ObserveGraphic"]');
+    this.graphicsMap = this.setGraphicsMap(graphics);
+  }
+
+  setGraphicsMap(graphics) {
+    return [].slice.call(graphics).reduce((map, graphic) => {
+      map[graphic.dataset.section] = graphic;
+      return map;
+    }, {})
+  }
+
+  beginObserve() {
     //create new Observer instance
-    let observer = new IntersectionObserver(this.onChange, {threshold: this.minThreshold});
+    let observer = new IntersectionObserver(this.onIntersection, {threshold: this.minThreshold});
 
     //start observing each graphic
-    graphics.forEach(graphic => observer.observe(graphic));
+    Object.values(this.graphicsMap).forEach(graphic => observer.observe(graphic));
   }
 
-  onChange = (observed) => {
+  onIntersection = (observed) => {
     let entry = observed[0];
-    let targetElement = entry.target;
+    let section = entry.target.dataset.section;
     let isIntersecting = entry.intersectionRatio > this.minThreshold;
-    let notAlreadyVisible = targetElement !== this.lastIntersectingEntry;
+    let notAlreadyVisible = section !== this.currentSection;
 
     if (isIntersecting && notAlreadyVisible) {
-      this.sectionHeading.innerHTML = data[targetElement.id].heading;
-      this.lastIntersectingEntry = targetElement;
+      this.currentSection = section;
+      this.changeHeading();
+      this.changeText();
+      this.changeGraphic();
     }
+  }
+
+  changeHeading() {
+    let sectionData = data[this.currentSection];
+    this.heading.innerHTML = sectionData.heading;
+  }
+
+  changeText() {
+    let sectionData = data[this.currentSection];
+    this.text.innerHTML = this.onMicrocosmView ? sectionData.microcosmText : sectionData.browserText;
+  }
+
+  changeGraphic() {
+    let section = this.currentSection;
+    let graphicElement = this.graphicsMap[section];
+    let newGraphicUrl = this.onMicrocosmView ? data[section].microcosmGraphicUrl : data[section].browserGraphicUrl
+
+    graphicElement.setAttribute('src', newGraphicUrl);
+  }
+
+  changeButtonText(button) {
+    button.classList.toggle('-browserView', !this.onMicrocosmView);
+  }
+
+  changeSubheadingText(button) {
+    this.subheadingTop.classList.toggle('-browserView', !this.onMicrocosmView);
+    this.subheadingBottom.classList.toggle('-browserView', !this.onMicrocosmView);
+  }
+
+  switchView = (e) => {
+    this.onMicrocosmView = !this.onMicrocosmView;
+
+    this.changeGraphic();
+    this.changeSubheadingText();
+    this.changeText();
+    this.changeButtonText(e.target);
   }
 
   render() {
@@ -42,13 +96,13 @@ export default class IndexPage extends React.Component {
       <div className="wrapper">
         <section className="section">
           <div className="section__content">
-            <h2 className="section__content__heading" id="section__content__heading">
+            <h2 className="section__content__heading" id="heading">
               <span>01.</span>
               Rendering a View
             </h2>
 
-            <h3 className="section__content__subheading">In Microcosm</h3>
-            <p className="section__content__text">
+            <h3 className="section__content__subheading -top" id="subheading-top">In</h3>
+            <p className="section__content__text" id="text">
               The <a href="TODO">Domains</a> are in charge of keeping state
               organized, and provide whatever data is necessary to the
               Presenter. A Presenter at it's core is a React Component, so it
@@ -56,7 +110,8 @@ export default class IndexPage extends React.Component {
               view.
             </p>
 
-            <h3 className="section__content__subheading">Meanwhile, in the browser...</h3>
+            <h3 className="section__content__subheading -bottom" id="subheading-bottom">Meanwhile, in</h3>
+            <button className="section__browser-btn" onClick={this.switchView}></button>
           </div>
 
           <div className="section__graphic">
