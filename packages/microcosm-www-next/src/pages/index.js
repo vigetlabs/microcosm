77654d7b5e06cd088b11d054f085f405a846e543
Chloe Hwang <c.chloeh@gmail.com>
2017-12-14 12:53:24 -0500

    restructured base layout for new design and did basic styling

--- packages/microcosm-www-next/src/pages/index.js
+++ packages/microcosm-www-next/src/pages/index.js
@@ -7,46 +7,29 @@ export default class IndexPage extends React.Component {
     super(props);
   }
 
-  componentDidMount() {
-    this.leftFigure = document.getElementsByClassName('figure__content__text')[0];
-    let sections = document.querySelectorAll('[data-module="ObserveSection"]');
+  // componentDidMount() {
+  //   this.leftFigure = document.getElementsByClassName('figure__content__text')[0];
+  //   let sections = document.querySelectorAll('[data-module="ObserveSection"]');
 
-    //create new Observer instance
-    let observer = new IntersectionObserver(this.onChange.bind(this), {threshold: 0.6});
+  //   //create new Observer instance
+  //   let observer = new IntersectionObserver(this.onChange.bind(this), {threshold: 0.6});
 
-    //start observing each section
-    sections.forEach(section => observer.observe(section));
-  }
+  //   //start observing each section
+  //   sections.forEach(section => observer.observe(section));
+  // }
 
-  onChange(observed) {
-    let entry = observed[0];
+  // onChange(observed) {
+  //   let entry = observed[0];
 
-    if (entry.isIntersecting) {
-      this.leftFigure.innerHTML = data[entry.target.id].browserText;
-    }
-  }
+  //   if (entry.isIntersecting) {
+  //     this.leftFigure.innerHTML = data[entry.target.id].browserText;
+  //   }
+  // }
 
   render() {
     return (
       <div className="wrapper">
-        <div className="col-left">
-          {/* <h2 className="section-title">
-            <span>01.</span>
-            Rendering a View
-          </h2> */}
-          <figure className="figure -left">
-            <img src="" className="figure__graphic" alt="TODO" />
-            <figcaption className="figure__content">
-              <h3 className="figure__content__header">In the browser</h3>
-              <p className="figure__content__text"></p>
-            </figcaption>
-          </figure>
-        </div>
-
-        <div className="col-right">
-          <Section1 />
-          <Section2 />
-        </div>
+        <Section1 />
       </div>
     );
   }
