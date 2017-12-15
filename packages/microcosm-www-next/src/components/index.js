123be04e773b3a341eb3452e2bb78805aca60f95
Chloe Hwang <c.chloeh@gmail.com>
2017-12-15 15:19:24 -0500

    ran yarn format

--- packages/microcosm-www-next/src/components/index.js
+++ packages/microcosm-www-next/src/components/index.js
@@ -1,5 +1,5 @@
-import Graphic1 from './graphic1';
-import Graphic2 from './graphic2';
-import Graphic3 from './graphic3';
+import Graphic1 from './graphic1'
+import Graphic2 from './graphic2'
+import Graphic3 from './graphic3'
 
-export { Graphic1, Graphic2, Graphic3 };
+export { Graphic1, Graphic2, Graphic3 }
--- packages/microcosm-www-next/src/pages/index.js
+++ packages/microcosm-www-next/src/pages/index.js
@@ -1,98 +1,110 @@
 import React from 'react'
-import data from '../data/index.json';
-import { Graphic1, Graphic2, Graphic3 } from '../components';
+import data from '../data/index.json'
+import { Graphic1, Graphic2, Graphic3 } from '../components'
 
 export default class IndexPage extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
 
   componentDidMount() {
     // TODO later - accessibility to alert on change
-    this.setVars();
-    this.beginObserve();
+    this.setVars()
+    this.beginObserve()
   }
 
   setVars() {
-    this.onMicrocosmView = true;
-    this.currentSection = 1;
+    this.onMicrocosmView = true
+    this.currentSection = 1
     this.observeOptions = {
       root: null,
       rootMargin: '0px 0px 100px',
-      threshold: 1.0
+      threshold: 1.0,
     }
 
-    this.text = document.getElementById('text');
-    this.heading = document.getElementById('heading');
-    this.subheadingTop = document.getElementById('subheading-top');
-    this.subheadingBottom = document.getElementById('subheading-bottom');
+    this.text = document.getElementById('text')
+    this.heading = document.getElementById('heading')
+    this.subheadingTop = document.getElementById('subheading-top')
+    this.subheadingBottom = document.getElementById('subheading-bottom')
 
-    let graphics = document.querySelectorAll('[data-module="ObserveGraphic"]');
-    this.graphicsMap = this.setGraphicsMap(graphics);
+    let graphics = document.querySelectorAll('[data-module="ObserveGraphic"]')
+    this.graphicsMap = this.setGraphicsMap(graphics)
   }
 
   setGraphicsMap(graphics) {
     return [].slice.call(graphics).reduce((map, graphic) => {
-      map[graphic.dataset.section] = graphic;
-      return map;
+      map[graphic.dataset.section] = graphic
+      return map
     }, {})
   }
 
   beginObserve() {
     //create new Observer instance
-    let observer = new IntersectionObserver(this.onIntersection, this.observeOptions);
+    let observer = new IntersectionObserver(
+      this.onIntersection,
+      this.observeOptions
+    )
 
     //start observing each graphic
-    Object.values(this.graphicsMap).forEach(graphic => observer.observe(graphic));
+    Object.values(this.graphicsMap).forEach(graphic =>
+      observer.observe(graphic)
+    )
   }
 
-  onIntersection = (observed) => {
-    let entry = observed[0];
-    let section = entry.target.dataset.section;
-    let notAlreadyVisible = section !== this.currentSection;
+  onIntersection = observed => {
+    let entry = observed[0]
+    let section = entry.target.dataset.section
+    let notAlreadyVisible = section !== this.currentSection
 
     if (entry.isIntersecting && notAlreadyVisible) {
-      this.currentSection = section;
-      this.changeHeading();
-      this.changeText();
-      this.changeGraphic();
+      this.currentSection = section
+      this.changeHeading()
+      this.changeText()
+      this.changeGraphic()
     }
   }
 
   changeHeading() {
-    let sectionData = data[this.currentSection];
-    this.heading.innerHTML = sectionData.heading;
+    let sectionData = data[this.currentSection]
+    this.heading.innerHTML = sectionData.heading
   }
 
   changeText() {
-    let sectionData = data[this.currentSection];
-    this.text.innerHTML = this.onMicrocosmView ? sectionData.microcosmText : sectionData.browserText;
+    let sectionData = data[this.currentSection]
+    this.text.innerHTML = this.onMicrocosmView
+      ? sectionData.microcosmText
+      : sectionData.browserText
   }
 
   changeGraphic() {
-    let section = this.currentSection;
-    let graphicElement = this.graphicsMap[section];
-    let newGraphicUrl = this.onMicrocosmView ? data[section].microcosmGraphicUrl : data[section].browserGraphicUrl
+    let section = this.currentSection
+    let graphicElement = this.graphicsMap[section]
+    let newGraphicUrl = this.onMicrocosmView
+      ? data[section].microcosmGraphicUrl
+      : data[section].browserGraphicUrl
 
-    graphicElement.setAttribute('src', newGraphicUrl);
+    graphicElement.setAttribute('src', newGraphicUrl)
   }
 
   changeButtonText(button) {
-    button.classList.toggle('-browserView', !this.onMicrocosmView);
+    button.classList.toggle('-browserView', !this.onMicrocosmView)
   }
 
   changeSubheadingText(button) {
-    this.subheadingTop.classList.toggle('-browserView', !this.onMicrocosmView);
-    this.subheadingBottom.classList.toggle('-browserView', !this.onMicrocosmView);
+    this.subheadingTop.classList.toggle('-browserView', !this.onMicrocosmView)
+    this.subheadingBottom.classList.toggle(
+      '-browserView',
+      !this.onMicrocosmView
+    )
   }
 
-  switchView = (e) => {
-    this.onMicrocosmView = !this.onMicrocosmView;
+  switchView = e => {
+    this.onMicrocosmView = !this.onMicrocosmView
 
-    this.changeGraphic();
-    this.changeSubheadingText();
-    this.changeText();
-    this.changeButtonText(e.target);
+    this.changeGraphic()
+    this.changeSubheadingText()
+    this.changeText()
+    this.changeButtonText(e.target)
   }
 
   render() {
@@ -105,7 +117,12 @@ export default class IndexPage extends React.Component {
               Rendering a View
             </h2>
 
-            <h3 className="section__content__subheading -top" id="subheading-top">In</h3>
+            <h3
+              className="section__content__subheading -top"
+              id="subheading-top"
+            >
+              In
+            </h3>
             <p className="section__content__text" id="text">
               The <a href="TODO">Domains</a> are in charge of keeping state
               organized, and provide whatever data is necessary to the
@@ -114,8 +131,16 @@ export default class IndexPage extends React.Component {
               view.
             </p>
 
-            <h3 className="section__content__subheading -bottom" id="subheading-bottom">Meanwhile, in</h3>
-            <button className="section__browser-btn" onClick={this.switchView}></button>
+            <h3
+              className="section__content__subheading -bottom"
+              id="subheading-bottom"
+            >
+              Meanwhile, in
+            </h3>
+            <button
+              className="section__browser-btn"
+              onClick={this.switchView}
+            />
           </div>
 
           <div className="section__graphic">
@@ -125,6 +150,6 @@ export default class IndexPage extends React.Component {
           </div>
         </section>
       </div>
-    );
+    )
   }
 }
