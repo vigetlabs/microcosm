5dad9fca7aff31b8fe15d6d6744cbe505415b2c9
Chloe Hwang <c.chloeh@gmail.com>
2017-12-15 15:09:02 -0500

    tweaked Intersection Observer to run smoother, added third section

---
+++ packages/microcosm-www-next/src/components/graphic3.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Graphic3 = ({ fill }) => (
+  <figure>
+    <img
+      src="http://via.placeholder.com/600x600"
+      alt="TODO"
+      className="section__graphic__figure"
+      data-module="ObserveGraphic"
+      data-section="3"
+    />
+  </figure>
+)
+
+export default Graphic3
