432bda334342922c73862221accef3124ce4d008
Chloe Hwang <c.chloeh@gmail.com>
2017-12-21 17:37:05 -0500

    added styling for desktop layout

--- packages/microcosm-www-next/src/components/graphic.js
+++ packages/microcosm-www-next/src/components/graphic.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const Graphic = ({ section, graphicUrl }) => (
-  <figure>
+  <figure
+    className="section__graphic__figure"
+    data-module="ObserveGraphic"
+    data-section={section}
+  >
     <img
       src={graphicUrl}
       alt="TODO"
-      className="section__graphic__figure"
-      data-module="ObserveGraphic"
-      data-section={section}
     />
   </figure>
 )
