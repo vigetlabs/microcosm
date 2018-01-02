1b5bc81eee9c3851befcf9a3669ce05ef99ec1bd
Chloe Hwang <c.chloeh@gmail.com>
2018-01-02 15:06:42 -0500

    tweaked side nav to work on scroll

--- packages/microcosm-www-next/src/components/graphic.js
+++ packages/microcosm-www-next/src/components/graphic.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Graphic = ({ section, graphicUrl }) => (
   <figure
+    id={section}
     className="section__graphic__figure"
     data-module="ObserveGraphic"
     data-section={section}
