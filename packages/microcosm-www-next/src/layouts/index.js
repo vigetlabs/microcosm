e9ac44933fff68bf4fc464ccd3f9874ad7ecaf25
Chloe Hwang <c.chloeh@gmail.com>
2018-01-03 17:24:57 -0500

    refactored json file to be used for side nav

--- packages/microcosm-www-next/src/layouts/index.js
+++ packages/microcosm-www-next/src/layouts/index.js
@@ -49,7 +49,7 @@ const PageWrapper = ({ children }) => (
       title="Microcosm"
       meta={[
         { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'keywords', content: 'sample, something' }
       ]}
     />
 
@@ -62,7 +62,7 @@ const PageWrapper = ({ children }) => (
 )
 
 PageWrapper.propTypes = {
-  children: PropTypes.func,
+  children: PropTypes.func
 }
 
 export default PageWrapper
