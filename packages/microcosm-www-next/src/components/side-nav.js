f40ae09e383c150d4c296474fff827ae1c74491f
Chloe Hwang <c.chloeh@gmail.com>
2018-01-03 17:24:57 -0500

    refactored json file to be used for side nav

--- packages/microcosm-www-next/src/components/side-nav.js
+++ packages/microcosm-www-next/src/components/side-nav.js
@@ -2,14 +2,13 @@ import React from 'react'
 import data from '../data/index.json'
 
 const SideNav = ({ graphics, currentSection }) => {
-
   const scrollToElement = (e, elem) => {
     e.preventDefault()
 
     window.scroll({
       top: elem.offsetTop - 400,
       left: 0,
-      behavior: 'smooth',
+      behavior: 'smooth'
     })
   }
 
@@ -17,21 +16,24 @@ const SideNav = ({ graphics, currentSection }) => {
     <aside className="section-nav">
       <nav>
         <ol className="section-nav__list">
-          {graphics.map(({ num, elem }) => {
-            return (
-              <li key={num} className="section-nav__list__link">
-                <a
-                  href={'#graphic-' + num}
-                  className={num === currentSection ? '-active' : ''}
-                  onClick={e => {
-                    scrollToElement(e, elem)
-                  }}
-                >
-                  {data[num].heading}
-                </a>
-              </li>
-            )
-          })}
+          {graphics.map(({ num, elem }) => (
+            <li
+              key={num}
+              className={
+                'section-nav__list__link' +
+                (num === currentSection ? ' -active' : '')
+              }
+            >
+              <a
+                href={'#graphic-' + num}
+                onClick={e => {
+                  scrollToElement(e, elem)
+                }}
+              >
+                {data[num].heading}
+              </a>
+            </li>
+          ))}
         </ol>
       </nav>
     </aside>
