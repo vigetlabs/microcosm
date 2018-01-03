2f8f0eb82ca32d98ac340c69eaf39e95863574ae
Chloe Hwang <c.chloeh@gmail.com>
2018-01-03 12:20:53 -0500

    removed prettier dev dep

--- packages/microcosm-www-next/src/layouts/index.js
+++ packages/microcosm-www-next/src/layouts/index.js
@@ -44,7 +44,7 @@ const PageWrapper = ({ children }) => (
       title="Microcosm"
       meta={[
         { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'keywords', content: 'sample, something' }
       ]}
     />
 
@@ -57,7 +57,7 @@ const PageWrapper = ({ children }) => (
 )
 
 PageWrapper.propTypes = {
-  children: PropTypes.func,
+  children: PropTypes.func
 }
 
 export default PageWrapper
