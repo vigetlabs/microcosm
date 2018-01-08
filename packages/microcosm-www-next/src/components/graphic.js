c2b66e4a02cb644f303f0c01c8f227162fc630dd
Chloe Hwang <c.chloeh@gmail.com>
2018-01-08 18:02:34 -0500

    laying out structure of browser graphic

--- packages/microcosm-www-next/src/components/graphic.js
+++ packages/microcosm-www-next/src/components/graphic.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { cat } from '../images'
 
 const Graphic = ({ section, microcosmView }) => (
   <figure
@@ -7,12 +8,29 @@ const Graphic = ({ section, microcosmView }) => (
     data-module="ObserveGraphic"
     data-section={section}
   >
-    <img
-      src={
-        microcosmView ? `/${section}-microcosm.png` : `/${section}-browser.png`
-      }
-      alt="TODO"
-    />
+    {
+      microcosmView ?
+        <img src={`/${section}-microcosm.png`} className="microcosm-graphic" alt="TODO" />
+      :
+        <div className="browser-graphic">
+          <header>
+            <p>Quizzfeed</p>
+          </header>
+          <main>
+            <div className="content">
+              <ol>
+                <li>Cool</li>
+                <li>Curious</li>
+                <li>Calm</li>
+                <li>Cautious</li>
+              </ol>
+            </div>
+            <div className="graphic">
+              <img src={cat} alt="TODO" />
+            </div>
+          </main>
+        </div>
+    }
   </figure>
 )
 
--- packages/microcosm-www-next/src/images/index.js
+++ packages/microcosm-www-next/src/images/index.js
@@ -1,5 +1,6 @@
 import menu from './menu.png'
+import cat from './cat.png'
 import VigetLogo from './viget-logo.js'
 import microcosmLogo from './microcosm-logo.png'
 
-export { menu, VigetLogo, microcosmLogo }
+export { menu, cat, VigetLogo, microcosmLogo }
