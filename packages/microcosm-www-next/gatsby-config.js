aae54b4d1933f39c763e00656b798736504bc0c5
Nathan Hunzaker <nate.hunzaker@gmail.com>
2018-01-09 15:57:21 -0500

    Add public path to new site deployment

--- packages/microcosm-www-next/gatsby-config.js
+++ packages/microcosm-www-next/gatsby-config.js
@@ -2,5 +2,6 @@ module.exports = {
   siteMetadata: {
     title: `Microcosm`
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`]
+  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`],
+  pathPrefix: process.env.PUBLIC_PATH || '/'
 }
