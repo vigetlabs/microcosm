dfea116ef45ca665971b85a6763a2c524359fc88
Chloe Hwang <chloe.hwang@viget.com>
2018-01-26 14:07:01 -0500

    set up flip animations (#472)
    * set up flip animations
    
    * ran yarn format, fixed formatting, added conditional to not flip bookend graphics
    
    * tweaked based on PR comments

--- packages/microcosm-www-next/src/components/graphic.js
+++ packages/microcosm-www-next/src/components/graphic.js
@@ -8,15 +8,25 @@ const Graphic = ({ section, microcosmView }) => (
     data-module="ObserveGraphic"
     data-section={section}
   >
-    {microcosmView || section === 0 || section == 9 ? (
-      <img
-        data-src={`/${section}-microcosm.png`}
-        className="lazyload microcosm-graphic"
-        alt="TODO"
-      />
-    ) : (
-      <BrowserGraphic />
-    )}
+    <div
+      className={
+        'flip-container' +
+        (microcosmView || section === 0 || section == 9 ? '' : ' -flipped')
+      }
+    >
+      <div className="flipper">
+        <div className="flipper__front">
+          <img
+            data-src={`/${section}-microcosm.png`}
+            className="lazyload microcosm-graphic"
+            alt="TODO"
+          />
+        </div>
+        <div className="flipper__back">
+          <BrowserGraphic />
+        </div>
+      </div>
+    </div>
   </figure>
 )
 
