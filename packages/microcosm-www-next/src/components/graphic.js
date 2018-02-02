35450ab959615319217f5b1b29ed66455da056c8
Chloe Hwang <c.chloeh@gmail.com>
2018-02-02 16:55:41 -0500

    switched from using IntersectionObserver to debounce, fixed bug with footer

--- packages/microcosm-www-next/src/components/graphic.js
+++ packages/microcosm-www-next/src/components/graphic.js
@@ -1,33 +1,50 @@
 import React from 'react'
 import BrowserGraphic from './browser-graphic'
 
-const Graphic = ({ section, microcosmView }) => (
-  <figure
-    id={'graphic-' + section}
-    className="section__graphic__figure"
-    data-module="ObserveGraphic"
-    data-section={section}
-  >
-    <div
-      className={
-        'flip-container' +
-        (microcosmView || section === 0 || section == 9 ? '' : ' -flipped')
-      }
-    >
-      <div className="flipper">
-        <div className="flipper__front">
-          <img
-            data-src={`/${section}-microcosm.png`}
-            className="lazyload microcosm-graphic"
-            alt="TODO"
-          />
-        </div>
-        <div className="flipper__back">
-          <BrowserGraphic />
-        </div>
-      </div>
-    </div>
-  </figure>
-)
+export default class Graphic extends React.Component {
 
-export default Graphic
+  shouldComponentUpdate(nextProps) {
+    let fadeClassChanged = nextProps.fadeClass !== this.props.fadeClass
+    let viewChanged = nextProps.microcosmView !== this.props.microcosmView
+
+    if (fadeClassChanged || viewChanged) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+  render() {
+    let section = this.props.section
+
+    return (
+      <figure
+        id={'graphic-' + section}
+        className={"section__graphic__figure " + this.props.fadeClass}
+        data-module="ObserveGraphic"
+        data-section={section}
+      >
+        <div
+          className={
+            'flip-container' +
+            (this.props.microcosmView || section === 0 || section == 9 ? '' : ' -flipped')
+          }
+        >
+          <div className="flipper">
+            <div className="flipper__front">
+              <img
+                data-src={`/${section}-microcosm.png`}
+                className="lazyload microcosm-graphic"
+                alt="TODO"
+              />
+            </div>
+            <div className="flipper__back">
+              <BrowserGraphic />
+            </div>
+          </div>
+        </div>
+      </figure>
+    )
+  }
+}
