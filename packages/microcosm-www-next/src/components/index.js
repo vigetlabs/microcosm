70386620b0381684364f957a3115a55fb928db20
Chloe Hwang <c.chloeh@gmail.com>
2018-02-05 10:04:19 -0500

    ran yarn format

--- packages/microcosm-www-next/src/components/index.js
+++ packages/microcosm-www-next/src/components/index.js
@@ -5,11 +5,4 @@ import Header from './header'
 import Subheading from './subheading'
 import ToggleContainer from './toggle-container'
 
-export {
-  Button,
-  Description,
-  Graphic,
-  Header,
-  Subheading,
-  ToggleContainer
-}
+export { Button, Description, Graphic, Header, Subheading, ToggleContainer }
