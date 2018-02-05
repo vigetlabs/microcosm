23ab8d5c076cf7ce392312d004150314d15ab894
Chloe Hwang <c.chloeh@gmail.com>
2018-02-05 10:03:02 -0500

    removed unnecessary files

--- packages/microcosm-www-next/src/components/index.js
+++ packages/microcosm-www-next/src/components/index.js
@@ -2,7 +2,6 @@ import Button from './button'
 import Description from './description'
 import Graphic from './graphic'
 import Header from './header'
-import SideNav from './side-nav'
 import Subheading from './subheading'
 import ToggleContainer from './toggle-container'
 
@@ -11,7 +10,6 @@ export {
   Description,
   Graphic,
   Header,
-  SideNav,
   Subheading,
   ToggleContainer
 }
