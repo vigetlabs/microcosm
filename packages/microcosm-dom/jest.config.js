36f393c44e49353239aed652f6a0deaff836d4b6
Nathan Hunzaker <nate.hunzaker@gmail.com>
2018-02-14 08:02:35 -0500

    Fix reference to bad jest config in microcosm-dom

--- packages/microcosm-dom/jest.config.js
+++ packages/microcosm-dom/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.js'],
   modulePathIgnorePatterns: ['build'],
-  projects: ['./jest.preact.config.js', './jest/react.config.js']
+  projects: ['./jest.preact.config.js', './jest.react.config.js']
 }
