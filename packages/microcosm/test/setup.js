0e5dede3d1ec0adf9609b59342cc803289956825
Nathan Hunzaker <nate.hunzaker@gmail.com>
2018-03-16 09:42:18 -0400

    Inline jest extensions

--- packages/microcosm/test/setup.js
+++ packages/microcosm/test/setup.js
@@ -1,4 +1,5 @@
-import '../src/addons/jest-matchers'
+import { get } from 'microcosm'
+import expect from 'expect'
 
 // Make a strict-only test flag
 it.dev = function(description, test) {
@@ -16,3 +17,45 @@ describe.dev = function(description, suite) {
 
   return describe.skip(description, test)
 }
+
+expect.extend({
+  toHaveStatus(action, status) {
+    let operator = this.isNot ? 'not to' : 'to'
+    let pass = action.status === status
+
+    return {
+      pass: pass,
+      message: () => {
+        return `Expected action ${operator} be '${status}'. Instead got ${
+          action.status
+        }.`
+      }
+    }
+  },
+
+  toHaveState(repo, key, value) {
+    let operator = this.isNot ? 'not to' : 'to'
+    let pass = false
+    let actual = get(repo.state, key)
+
+    if (arguments.length > 2) {
+      pass = JSON.stringify(actual) === JSON.stringify(value)
+    } else {
+      pass = actual !== undefined
+    }
+
+    // Display friendly key path
+    let path = [].concat(key).join('.')
+
+    return {
+      pass: pass,
+      message: () => {
+        return (
+          `Expected '${path}' in repo.state ${operator} be ${this.utils.printExpected(
+            value
+          )} ` + `but it is ${this.utils.printReceived(actual)}.`
+        )
+      }
+    }
+  }
+})
