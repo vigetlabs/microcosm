646a99e0fe51e7848552b148ddd303eb6a54e997
leobauza <leonardobauza@gmail.com>
2018-04-19 14:32:29 -0400

    fix unused import error

--- packages/microcosm-www-next/src/layouts/index.js
+++ packages/microcosm-www-next/src/layouts/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
-import { menu, VigetLogo, microcosmLogo } from '../images'
+import { VigetLogo, microcosmLogo } from '../images'
 import '../stylesheets/app.scss'
 
 const isBrowser = typeof window !== 'undefined'
